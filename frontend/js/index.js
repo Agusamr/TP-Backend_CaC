0170438ff191ef9eb3d128477a2bca3406f6cbb1
MGDDS <MGDDS@users.noreply.github.com>
2024-07-10 22:42:07 +0000

    Workin ok DELETE

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,7 +1,9 @@
 const form = document.getElementById("crear_pizza_form")
 const btn = document.getElementById("btn-post")
 
+//const URL_API_BASE = "http://127.0.0.1:5000/api/pelicula/"
 const URL_API_BASE = "http://127.0.0.1:8000/lista-de-pizzas/pizza/"
+
 
 btn.addEventListener(
     "click", (e) => {
@@ -13,137 +15,67 @@ btn.addEventListener(
         .catch(error => console.log({error}))
     }
 )
-// const btnGet = document.getElementById("btn-get")
-// btnGet.addEventListener(
-//     "click", () => {
-//         fetch(URL_API_BASE)
-//         .then(res => res.json())
-//         .then(data =>console.log(data))
-//         .catch(error => console.log({error}))
-//     }
-// )
+
 class Pizza{
     constructor({id,nombre,tamanio,precio}){
-        
-        this.id = id
+        this.id=id
         this.nombre = nombre
         this.tamanio = tamanio
         this.precio = precio
-        // this.url = "http://127.0.0.1:8000/lista-de-pizzas/pizza/"
-        
+     //   this.url = "http://127.0.0.1:5000/api/pelicula/"        
+        this.url = "http://127.0.0.1:8000/lista-de-pizzas/pizza/"
     }
-    
-//     createDiv(){
-//         let div = document.createElement("div");
 
-//         this.div.innerHTML =  `
-//             <div id="${this.id}">
-//                 <h4>${this.nombre}</h4>
-//                 <hr>
-//                 <ul>
-//                     <li>tamaño: ${this.tamanio}</li>
-//                     <li>precio: ${this.precio}</li>
-                    
-//                 </ul>
-//             </div>
-//         `;
-        
-//     }
-// }
+    createDiv(){
+        this.div = document.createElement("div")
 
-// const btnGet = document.getElementById("btn-get")
-// btnGet.addEventListener(
-//     "click", () => {
-//         fetch(URL_API_BASE)
-//         .then(res => res.json())
-//         .then(data =>{
-//             let ingesta_data = data
-//             .map(p => new Pizza(p))
-//             .reduce(
-//                 (acc, pizza) =>  acc + pizza.createDiv(),
-//             ) 
-            
-//                 document.getElementById("pizzas").innerHTML = ingesta_data
-                
-        
-//             // let container = document.getElementById("pizza")
-//             // data.map(p =>  new Pizza(p))
-//             // .forEach(p => container.appendChild(p.createDiv()))
-            
-//         } 
-//         )
-//         .catch(error => console.log({error}))
-//     }
-// )
-createDiv() {
-    let div = document.createElement("div");
+        this.div.innerHTML =  `
+            <div id="${this.id}">
+                <h4>${this.nombre}</h4>
+                <hr>
+                <ul>
+                    <li>nombre: ${this.nombre}</li>
+                    <li>tamanio: ${this.tamanio}</li>
+                    <li>precio: ${this.precio}</li>
+                </ul>
+            </div>
+        `
 
-    div.innerHTML = `
-        <div id="${this.id}">
-            <h4>${this.nombre}</h4>
-            <hr>
-            <ul>
-                <li>tamaño: ${this.tamanio}</li>
-                <li>precio: ${this.precio}</li>
-            </ul>
-            
-        </div>
-    `;
-
-    return div; // Devolver el elemento div creado
-}
-}
-
-const btnGet = document.getElementById("btn-get");
-btnGet.addEventListener("click", () => {
-fetch(URL_API_BASE)
-    .then(res => res.json())
-    .then(data => {
-        let fragment = document.createDocumentFragment();
-
-        data.forEach(pizzaData => {
-            let pizza = new Pizza(pizzaData);
-            let pizzaDiv = pizza.createDiv();
-            fragment.appendChild(pizzaDiv);
-        });
-
-        let pizzasContainer = document.getElementById("pizzas");
-        pizzasContainer.innerHTML = ''; // Limpiar antes de agregar
-        pizzasContainer.appendChild(fragment); // Agregar todos los elementos al contenedor
-    })
-    .catch(error => console.error(error));
-});
-
-const btnDelete = document.getElementById("btn-delete");
-
-btnGet.addEventListener("click", () => {
-    fetch(URL_API_BASE)
-        .then(res => res.json())
-        .then(data => {
-            let fragment = document.createDocumentFragment();
-
-            data.forEach(pizzaData => {
-                let pizza = new Pizza(pizzaData);
-                let pizzaDiv = pizza.createDiv();
-                fragment.appendChild(pizzaDiv);
-            });
-
-            let pizzasContainer = document.getElementById("pizzas");
-            pizzasContainer.innerHTML = ''; // Limpiar antes de agregar
-            pizzasContainer.appendChild(fragment); // Agregar todos los elementos al contenedor
+        let btn = document.createElement("button")
+        btn.innerText = `Borrar Pizza ${this.nombre}`
+        btn.addEventListener("click", ()=>{
+            // fetch(`${this.url}${this.id}/`,{method:"DELETE"})
+            fetch(`http://127.0.0.1:8000/lista-de-pizzas/pizza/${this.id}/`, {
+                method: 'DELETE'
+            })
+            .then(this.removeDisplay())
+            .catch(error => console.log({error}))
         })
-        .catch(error => console.error(error));
-});
+        this.div.appendChild(btn)
+        return this.div
 
-btnDelete.addEventListener("click", () => {
-    let pizzasContainer = document.getElementById("pizzas");
-    let lastPizza = pizzasContainer.lastChild;
-
-    if (lastPizza) {
-        pizzasContainer.removeChild(lastPizza);
-    } else {
-        console.log("No hay elementos para eliminar.");
     }
-});
+  
+    removeDisplay() {
+        this.div.remove();
+    }
 
 
+
+}
+
+
+const btnGet = document.getElementById("btn-get")
+btnGet.addEventListener(
+    "click", () => {
+        fetch(URL_API_BASE)
+        .then(res => res.json())
+        .then(data =>{
+            let container = document.getElementById("pizzas")
+            data.map(p =>  new Pizza(p))
+            .forEach(p => container.appendChild(p.createDiv()))
+        } 
+        )
+        .catch(error => console.log({error}))
+    }
+)
